ca82cd4175a0a360fb3b85235233293ee310910e
YQ-China <918674119@qq.com>
2020-05-24 16:52:04 +0800

    添加文件

---
+++ HelloGit/src/main/java/gold/anime/git/HelloGit.java
@@ -0,0 +1,12 @@
+package gold.anime.git;
+
+/**
+ * @Author: 南黎丶
+ * @Date: 2020/5/24
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
